9bb6c8a3e5892d9c6aec87c947d16aa7f610edd2
areeshy-ITdeveloper <areeshyit@gmail.com>
2026-09-06 19:24:56 +0500

    Save changes before rebase

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'multi-platform-content-calendar',
+  base: '/socialhub/',
   server: {
     port: 3000, // Yahan apna marzi ka port number likhein (e.g. 3000, 8080)
   }
